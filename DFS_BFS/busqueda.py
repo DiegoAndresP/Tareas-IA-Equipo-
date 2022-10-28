ee2a6b3d7159fdc795286eb8e5463c9374fa5de4
Kenneth Rodriguez <kennethrdz@hotmail.com>
2022-10-27 22:49:15 -0500

    Agregar medición de tiempo en algoritmos de búsqueda BFS y DFS. Modificar funciones DFS y BFS para que regresen el recorrido seguido hasta encontrar un nodo en particular. Eliminar librerías importadas sin usar de minimax.py

--- DFS_BFS/busqueda.py
+++ DFS_BFS/busqueda.py
@@ -1,35 +1,43 @@
 from instancias import generar_instancias
+import time
 
 def DFS(grafo: dict, inicio: int, meta: int, visitados = []):
-    print(inicio)
-    if inicio == meta: 
-        return inicio
-    print("->")
     visitados.append(inicio)
-    nodo = inicio
+    if inicio == meta: 
+        return visitados
     for adyacente in grafo[inicio]: 
         if adyacente not in visitados: 
-            nodo = DFS(grafo, adyacente, meta, visitados)
-            if nodo == meta: 
+            DFS(grafo, adyacente, meta, visitados)
+            if meta in visitados: 
                 break
-    return nodo
+    return visitados
 
 def BFS(grafo: dict, inicio: int, meta: int): 
-    visitados = [inicio]
+    visitados = []
     por_visitar = [inicio]
     while len(por_visitar) > 0: 
         nodo = por_visitar.pop(0)
-        print(nodo)
+        visitados.append(nodo)
+        #print(nodo)
         if nodo == meta: 
-            return
-        else: 
-            print("->")
+            return visitados
+        #else: 
+        #    print("->")
         for n in grafo[nodo]: 
-            if n not in visitados: 
-                visitados.append(n)
+            if n not in visitados and n not in por_visitar: 
                 por_visitar.append(n)
+    return visitados
 
 if __name__=="__main__": 
     grafo = generar_instancias()
-    #BFS(grafo, 1, 5)
-    DFS(grafo, 1, 5)
+    meta = int(input("Nodo que desea buscar? "))
+    print("-"*10)
+    print("Breadth First Search")
+    tiempo_inicial = time.time()
+    print("Recorrido: "+str(BFS(grafo, 1, meta)))
+    print("Tiempo de búsqueda: "+str(time.time()-tiempo_inicial)+"s")
+    print("-"*10)
+    print("Depth First Search")
+    tiempo_inicial = time.time()
+    print("Recorrido: "+str(DFS(grafo, 1, meta)))
+    print("Tiempo de búsqueda: "+str(time.time()-tiempo_inicial)+"s")
