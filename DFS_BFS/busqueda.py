092ae3861008e043d58a1f9b62c88c92804006a3
Kenneth Rodriguez <kennethrdz@hotmail.com>
2022-11-19 18:43:15 -0600

    Fixes

--- DFS_BFS/busqueda.py
+++ DFS_BFS/busqueda.py
@@ -1,83 +1,140 @@
 from instancias import generar_instancias
-import time
+from time import time
+from math import inf
 
-def DFS(grafo: dict, meta: int, inicio = 1, visitados = []):
-    visitados.append(inicio)
-    if inicio == meta: 
-        return visitados
-    for adyacente in grafo[inicio]: 
-        if adyacente not in visitados: 
-            DFS(grafo, adyacente, meta, visitados)
-            if meta in visitados: 
-                break
-    return visitados
+def validar_sn(sn)->bool: 
+    return sn == "s" or sn == "n"
 
-def DFS_con_peso(grafo: dict, meta: int, inicio = 1, peso = 0, visitados = [], ruta = []): 
-    visitados.append(inicio)
-    ruta.append((inicio, peso))
-    if inicio == meta: 
-        return ruta
-    for adyacente in grafo[inicio]: 
-        if adyacente[0] not in visitados: 
-            DFS_con_peso(grafo, meta, adyacente[0], adyacente[1], visitados, ruta)
-            if meta in visitados: 
+def validar_dirigido()->bool: 
+    dirigido = input("El grafo será dirigido? S/n: ").strip().lower()
+    while not validar_sn(dirigido): 
+        dirigido = input("Ingrese únicamente \"S\" o \"N\": ").lower()
+    return dirigido == "s"
+
+def validar_peso()->bool: 
+    peso = input("Desea que el grafo tenga peso? S/n: ").strip().lower()
+    while not validar_sn(peso): 
+        peso = input("Ingrese únicamente \"S\" o \"N\": ").lower().strip()
+    return peso == "s"
+
+def validar_int(frase: str)->int: 
+    num = 0
+    while num <= 0: 
+        try: 
+            num = int(input(frase))
+        except ValueError: 
+            print("Entrada inválida, ingrese un valor entero mayor a cero")
+            num = 0
+    return num
+
+def distancia_BFS(grafo: dict, ruta: list): 
+    distancia = 0
+    cola = [ruta[0]]
+    usados = []
+    while len(cola) > 0:
+        for v in grafo[cola[0]]:
+            if v[0] not in cola and v[0] not in usados and v[0] in ruta:
+                distancia += v[1]
+                cola.append(v[0])
+        usados.append(cola.pop(0))
+    return distancia
+
+def distancia_DFS(grafo: dict, ruta: list): 
+    distancia = 0
+    pila = [ruta[0]]
+    usados = []
+    while len(pila) > 0: 
+        terminal = True
+        for v in grafo[pila[len(pila)-1]]: 
+            if v[0] not in pila and v[0] not in usados and v[0] in ruta: 
+                distancia += v[1]
+                pila.append(v[0])
+                terminal = False
                 break
+        if terminal: 
+            usados.append(pila.pop())
+    return distancia
+
+def BFS(grafo: dict, meta: int, peso: bool, inicio = 1): 
+    cola = [inicio]
+    ruta = []
+    while len(cola) > 0: 
+        vertice = cola.pop(0)
+        ruta.append(vertice)
+        if vertice == meta: 
+            break
+        for v in grafo[vertice]: 
+            if not peso: 
+                temp = v
+            else: 
+                temp = v[0]
+            if temp not in ruta and temp not in cola: 
+                cola.append(temp)
     return ruta
 
-def BFS(grafo: dict, meta: int, inicio = 1): 
-    visitados = []
-    por_visitar = [inicio]
-    while len(por_visitar) > 0: 
-        nodo = por_visitar.pop(0)
-        visitados.append(nodo)
-
-        if nodo == meta: 
-            return visitados
-
-        for n in grafo[nodo]: 
-            if n not in visitados and n not in por_visitar:  
-                    por_visitar.append(n)
-    return visitados
-
-def BFS_con_peso(grafo: dict, meta: int, inicio = 1): 
-    visitados = []
-    ruta = []
-    nodos_por_visitar = [(inicio, 0)]
-    por_visitar = [inicio]
-    while len(nodos_por_visitar) > 0: 
-        nodo = nodos_por_visitar.pop(0)
-        por_visitar.pop(0)
-        visitados.append(nodo[0])
-        ruta.append(nodo)
-
-        if nodo[0] == meta: 
-            return ruta
-        
-        for n in grafo[nodo[0]]:
-            if n[0] not in visitados and n[0] not in por_visitar: 
-                nodos_por_visitar.append(n)
-                por_visitar.append(n[0])
+def DFS(grafo: dict, meta: int, peso: bool, inicio = 1, ruta = []): 
+    #pila.append(inicio)
+    #ruta.append(inicio)
+    ruta.append(inicio)
+    #ruta.append(pila[len(pila)-1])
+    if inicio == meta: 
+        return ruta
+    for v in grafo[inicio]: 
+        if not peso: 
+            temp = v
+        else: 
+            temp = v[0]
+        if temp not in ruta: 
+            ruta = DFS(grafo, meta, peso, temp, ruta)
+        if ruta[len(ruta)-1] == meta: 
+            break
     return ruta
 
 if __name__=="__main__": 
-    grafo = generar_instancias()
+    dirigido = validar_dirigido()
+    #dirigido = True
+    print("-"*5)
+    
+    peso = validar_peso()
+    #peso = False
+    print("-"*5)
+    
+    num_nodos = validar_int("Ingrese el número de nodos que contendrá el grafo: ")
+    print("-"*5)
+    
+    k = validar_int("Ingrese el número mínimo de nodos adyacentes: ")
+    print("-"*5)
 
+    #grafo = { 1: [6, 8, 10], 2: [5], 3: [8, 4, 1], 4: [7, 6, 1], 5: [6, 9, 1], 6: [7, 4, 9], 7: [9, 3, 4], 8: [5, 10, 2], 9: [4, 10], 10: [2, 3, 6] }
+
+    grafo = generar_instancias(dirigido, peso, num_nodos, k)
+    
+    print("-"*5)
+    
     meta = int(input("Nodo que desea buscar? "))
-
 
     print("-"*10)
     print("Breadth First Search")
-    tiempo_inicial = time.time()
-    recorrido = BFS(grafo, meta) if type(grafo[1][0]) != tuple else BFS_con_peso(grafo, meta)
-    print(recorrido)
-    print("Tiempo de búsqueda: "+str(time.time()-tiempo_inicial)+"s")
-    
-    
+    tiempo_inicial = time()
+    ruta = BFS(grafo, meta, peso)
+    print("Recorrido: "+str(ruta))
+    print("Nodos visitados: "+str(len(ruta)))
+    if meta not in ruta: 
+        print("No se encontró el vértice: "+str(meta))
+    print("Tiempo de búsqueda: "+str(time()-tiempo_inicial)+"s")
+    if peso: 
+        print("Distancia: "+str(distancia_BFS(grafo, ruta)))
+
     print("-"*10)
     print("Depth First Search")
-
-    tiempo_inicial = time.time()
-    recorrido = DFS(grafo, meta) if type(grafo[1][0]) != tuple else DFS_con_peso(grafo, meta)
-    print(recorrido)
-    print("Tiempo de búsqueda: "+str(time.time()-tiempo_inicial)+"s")
+    tiempo_inicial = time()
+    ruta = DFS(grafo, meta, peso)
+    print("Recorrido: "+str(ruta))
+    print("Nodos visitados: "+str(len(ruta)))
+    if meta not in ruta: 
+        print("No se encontró el vértice: "+str(meta))
+    print("Tiempo de búsqueda: "+str(time()-tiempo_inicial)+"s")
+    if peso: 
+        print("Distancia: "+str(distancia_DFS(grafo, ruta)))
+    
     
--- DFS_BFS/instancias.py
+++ DFS_BFS/instancias.py
@@ -1,43 +1,24 @@
 from random import randint
 
-def validar_sn(sn): 
-    return sn == "s" or sn == "n"
 
-def validar_int(frase: str)->int: 
-    while True: 
-        try: 
-            num = int(input(frase))
-        except ValueError: 
-            print("Entrada inválida, ingrese un valor entero")
-        else: 
-            return num
 
-def generar_instancias(): 
-    dirigido = input("El grafo será dirigido? S/n: ").lower()
-    while not validar_sn(dirigido): 
-        dirigido = input("Ingrese únicamente \"S\" o \"N\": ").lower()
-    peso = input("Desea que el grafo tenga peso? S/n: ")
-    while not validar_sn(peso): 
-        peso = input("Ingrese únicamente \"S\" o \"N\": ").lower()
-    num_nodos = validar_int("Ingrese el número de nodos que contendrá el grafo: ")
-    k = validar_int("Ingrese el número mínimo de nodos adyacentes: ")
-    peso_maximo = 100
+def generar_instancias(dirigido: bool, peso: bool, num_nodos: int, k: int, peso_maximo = 100): 
     nodos = dict()
     for i in range(num_nodos): 
         nodos[i+1] = []
 
-    if peso == "n": 
+    if not peso: 
         for nodo in nodos: 
             for i in range(k): 
                 adyacente = randint(1, len(nodos))
                 if adyacente != nodo and adyacente not in nodos[nodo]: 
                     nodos[nodo].append(adyacente)
-                    if dirigido == "n": 
+                    if not dirigido: 
                         nodos[adyacente].append(nodo)
     else: 
         for nodo in nodos: 
             disponibles = [i+1 for i in range(num_nodos)]
-            if dirigido == "n": 
+            if not dirigido: 
                 for num in nodos[nodo]: 
                     disponibles.remove(num[0])
             for i in range(k): 
@@ -45,7 +26,7 @@ def generar_instancias():
                 p = randint(1, peso_maximo)
                 if adyacente != nodo and adyacente in disponibles: 
                     nodos[nodo].append((adyacente, p))
-                    if dirigido == "n": 
+                    if not dirigido: 
                         nodos[adyacente].append((nodo, p))
                     disponibles.remove(adyacente)
         
@@ -54,46 +35,3 @@ def generar_instancias():
     for key in nodos: 
         print(str(key)+": "+str(nodos[key]))
     return nodos
-
-
-if __name__=="__main__": 
-    dirigido = input("El grafo será dirigido? S/n: ").lower()
-    while not validar_sn(dirigido): 
-        dirigido = input("Ingrese únicamente \"S\" o \"N\": ").lower()
-    peso = input("Desea que el grafo tenga peso? S/n: ")
-    while not validar_sn(peso): 
-        peso = input("Ingrese únicamente \"S\" o \"N\": ").lower()
-    num_nodos = validar_int("Ingrese el número de nodos que contendrá el grafo: ")
-    k = validar_int("Ingrese el número mínimo de nodos adyacentes: ")
-    peso_maximo = 100
-    nodos = dict()
-    for i in range(num_nodos): 
-        nodos[i+1] = []
-
-    if peso == "n": 
-        for nodo in nodos: 
-            for i in range(k): 
-                adyacente = randint(1, len(nodos))
-                if adyacente != nodo and adyacente not in nodos[nodo]: 
-                    nodos[nodo].append(adyacente)
-                    if dirigido == "n": 
-                        nodos[adyacente].append(nodo)
-    else: 
-        for nodo in nodos: 
-            disponibles = [i+1 for i in range(num_nodos)]
-            if dirigido == "n": 
-                for num in nodos[nodo]: 
-                    disponibles.remove(num[0])
-            for i in range(k): 
-                adyacente = randint(1, len(nodos))
-                p = randint(1, peso_maximo)
-                if adyacente != nodo and adyacente in disponibles: 
-                    nodos[nodo].append((adyacente, p))
-                    if dirigido == "n": 
-                        nodos[adyacente].append((nodo, p))
-                    disponibles.remove(adyacente)
-        
-
-
-    for key in nodos: 
-        print(str(key)+": "+str(nodos[key]))
